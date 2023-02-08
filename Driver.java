97d69e6de5659132ab51f8676ba772861deff7a9
Yuxiao Shen <113876235+Yuxiao-S@users.noreply.github.com>
2023-02-08 12:56:25 -0700

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,60 @@
+
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Yuxiao Shen
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		new SortManager(arg);
+
+		
+		while (scanner.hasNext()) {
+
+			String str = scanner.next();
+		
+			
+		public static Shape[] {
+			
+/*		if (str.equals(“Cylinder”) {
+			   Cylinder c = new Cylinder();
+			}
+			else if (str.equals(“Cone”) {
+			   Cone c = new Cone();
+			}
+			else if (str.equals(“Equilateral Triangle Prism”) {
+				EquilateralTrianglePrism e = new EquilateralTrianglePrism();
+			}
+			else if (str.equals(“Octagon Prism”) {
+				OctagonPrism o = new OctagonPrism();
+				}
+			else if (str.equals(“Pentagon Prism”) {
+				PentagonPrism p = new PentagonPrism();
+				}
+			else if (str.equals(“Prism") {
+				Prism p = new Prism();
+				}
+			else if (str.equals(“Pyramid") {
+				Pyramid p = new Pyramid();
+				}
+			else if (str.equals(“Square Prism”) {
+				SquarePrism p = new SquarePrism();
+				}
+			else {
+				
+			}*/
+		}
+		}
+		
+		public Comparator<Shape> getComparetor(char input){
+		
+		}
+	}
+}
